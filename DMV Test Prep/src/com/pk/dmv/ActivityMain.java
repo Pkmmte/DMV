8818c1d15f4db1194e1fe041685b7939e81a6984
Pkmmte <pkx7225@gmail.com>
2013-11-27 17:18:41 -0800

    Initial Clean Up

--- DMV Test Prep/src/com/pk/dmv/ActivityMain.java
+++ DMV Test Prep/src/com/pk/dmv/ActivityMain.java
@@ -14,9 +14,7 @@ public class ActivityMain extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
-
 }
