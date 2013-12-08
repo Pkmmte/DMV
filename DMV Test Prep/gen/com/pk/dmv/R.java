fc92b8a6170e16d4badb4ce0d8dfb5f5f52ab26e
Pkmmte <pkx7225@gmail.com>
2013-12-07 20:49:45 -0800

    Styles Stuff

--- DMV Test Prep/gen/com/pk/dmv/R.java
+++ DMV Test Prep/gen/com/pk/dmv/R.java
@@ -17,6 +17,7 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
+        public static final int ab_menu_divider=0x7f050015;
         public static final int app_background=0x7f050014;
         public static final int black=0x7f050012;
         public static final int black_trans=0x7f050013;
@@ -40,7 +41,7 @@ public final class R {
         public static final int holo_purple_light=0x7f05000e;
         public static final int holo_red_dark=0x7f050006;
         public static final int holo_red_light=0x7f050005;
-        public static final int pk_black=0x7f050015;
+        public static final int pk_black=0x7f050016;
         /**  Standard Colors 
          */
         public static final int transparent=0x7f050010;
@@ -55,50 +56,54 @@ public final class R {
         public static final int menu_card_top_margin=0x7f060003;
     }
     public static final class drawable {
-        public static final int card_bg=0x7f020000;
-        public static final int card_bg_focused=0x7f020001;
-        public static final int card_bg_pressed=0x7f020002;
-        public static final int card_black_focused=0x7f020003;
-        public static final int card_black_pressed=0x7f020004;
-        public static final int card_blue_dark_focused=0x7f020005;
-        public static final int card_blue_dark_pressed=0x7f020006;
-        public static final int card_focused=0x7f020007;
-        public static final int card_green_dark_focused=0x7f020008;
-        public static final int card_green_dark_pressed=0x7f020009;
-        public static final int card_orange_dark_focused=0x7f02000a;
-        public static final int card_orange_dark_pressed=0x7f02000b;
-        public static final int card_pressed=0x7f02000c;
-        public static final int card_purple_focused=0x7f02000d;
-        public static final int card_purple_light_focused=0x7f02000e;
-        public static final int card_purple_light_pressed=0x7f02000f;
-        public static final int card_purple_pressed=0x7f020010;
-        public static final int card_red_dark_focused=0x7f020011;
-        public static final int card_red_dark_pressed=0x7f020012;
-        public static final int card_selector_black_transparent=0x7f020013;
-        public static final int card_selector_blue_dark_transparent=0x7f020014;
-        public static final int card_selector_blue_light=0x7f020015;
-        public static final int card_selector_blue_light_transparent=0x7f020016;
-        public static final int card_selector_green_dark_transparent=0x7f020017;
-        public static final int card_selector_orange_dark_transparent=0x7f020018;
-        public static final int card_selector_purple_light_transparent=0x7f020019;
-        public static final int card_selector_purple_transparent=0x7f02001a;
-        public static final int card_selector_red_dark_transparent=0x7f02001b;
-        public static final int ic_action_info=0x7f02001c;
-        public static final int ic_contribute=0x7f02001d;
-        public static final int ic_find_local=0x7f02001e;
-        public static final int ic_flash_cards=0x7f02001f;
-        public static final int ic_handbook=0x7f020020;
-        public static final int ic_header_logo=0x7f020021;
-        public static final int ic_launcher=0x7f020022;
-        public static final int ic_practice_test=0x7f020023;
-        public static final int ic_settings=0x7f020024;
-        public static final int ic_signs=0x7f020025;
-        public static final int ic_test_history=0x7f020026;
-        public static final int ic_test_overview=0x7f020027;
-        public static final int ic_transparent=0x7f020028;
-        public static final int pic0=0x7f020029;
-        public static final int pic1=0x7f02002a;
-        public static final int pic2=0x7f02002b;
+        public static final int action_popup_background=0x7f020000;
+        public static final int card_bg=0x7f020001;
+        public static final int card_bg_focused=0x7f020002;
+        public static final int card_bg_pressed=0x7f020003;
+        public static final int card_black_focused=0x7f020004;
+        public static final int card_black_pressed=0x7f020005;
+        public static final int card_blue_dark_focused=0x7f020006;
+        public static final int card_blue_dark_pressed=0x7f020007;
+        public static final int card_focused=0x7f020008;
+        public static final int card_green_dark_focused=0x7f020009;
+        public static final int card_green_dark_pressed=0x7f02000a;
+        public static final int card_orange_dark_focused=0x7f02000b;
+        public static final int card_orange_dark_pressed=0x7f02000c;
+        public static final int card_pressed=0x7f02000d;
+        public static final int card_purple_focused=0x7f02000e;
+        public static final int card_purple_light_focused=0x7f02000f;
+        public static final int card_purple_light_pressed=0x7f020010;
+        public static final int card_purple_pressed=0x7f020011;
+        public static final int card_red_dark_focused=0x7f020012;
+        public static final int card_red_dark_pressed=0x7f020013;
+        public static final int card_selector_black_transparent=0x7f020014;
+        public static final int card_selector_blue_dark_transparent=0x7f020015;
+        public static final int card_selector_blue_light=0x7f020016;
+        public static final int card_selector_blue_light_transparent=0x7f020017;
+        public static final int card_selector_green_dark_transparent=0x7f020018;
+        public static final int card_selector_orange_dark_transparent=0x7f020019;
+        public static final int card_selector_purple_light_transparent=0x7f02001a;
+        public static final int card_selector_purple_transparent=0x7f02001b;
+        public static final int card_selector_red_dark_transparent=0x7f02001c;
+        public static final int ic_action_info=0x7f02001d;
+        public static final int ic_action_overflow=0x7f02001e;
+        public static final int ic_action_previous=0x7f02001f;
+        public static final int ic_contribute=0x7f020020;
+        public static final int ic_find_local=0x7f020021;
+        public static final int ic_flash_cards=0x7f020022;
+        public static final int ic_handbook=0x7f020023;
+        public static final int ic_header_logo=0x7f020024;
+        public static final int ic_launcher=0x7f020025;
+        public static final int ic_practice_test=0x7f020026;
+        public static final int ic_settings=0x7f020027;
+        public static final int ic_signs=0x7f020028;
+        public static final int ic_test_history=0x7f020029;
+        public static final int ic_test_overview=0x7f02002a;
+        public static final int ic_transparent=0x7f02002b;
+        public static final int pic0=0x7f02002c;
+        public static final int pic1=0x7f02002d;
+        public static final int pic2=0x7f02002e;
+        public static final int selector_blue_light_background=0x7f02002f;
     }
     public static final class id {
         public static final int Card=0x7f0a0004;
@@ -123,6 +128,7 @@ public final class R {
     }
     public static final class menu {
         public static final int activity_main=0x7f090000;
+        public static final int activity_practice_test=0x7f090001;
     }
     public static final class string {
         public static final int action_info=0x7f070002;
@@ -150,12 +156,26 @@ public final class R {
         public static final int test_overview_description=0x7f070006;
     }
     public static final class style {
-        public static final int ActionBarStyle_Transparent=0x7f080002;
-        public static final int ActionBarStyle_Transparent_TitleTextStyle=0x7f080003;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
+        public static final int ActionBarStyle_BlueLight=0x7f080001;
+        public static final int ActionBarStyle_DropDownListView=0x7f080007;
+        public static final int ActionBarStyle_Overflow=0x7f080004;
+        public static final int ActionBarStyle_PopupMenu=0x7f080006;
+        public static final int ActionBarStyle_SubTitleTextStyle=0x7f080003;
+        /**  Generic Stuff 
          */
-        public static final int AppTheme=0x7f080000;
-        public static final int TransparentTheme=0x7f080001;
+        public static final int ActionBarStyle_TitleTextStyle=0x7f080002;
+        /**  ActionBar Themes 
+         */
+        public static final int ActionBarStyle_Transparent=0x7f080000;
+        public static final int ActionBarStyle_WidgetTheme=0x7f080005;
+        /**  Default Theme 
+         */
+        public static final int AppTheme=0x7f080008;
+        /**  Practice Test Theme 
+         */
+        public static final int BlueLightTheme=0x7f08000a;
+        /**  Main Menu Theme 
+         */
+        public static final int TransparentTheme=0x7f080009;
     }
 }
--- DMV Test Prep/src/com/pk/dmv/activity/ActivityPracticeTest.java
+++ DMV Test Prep/src/com/pk/dmv/activity/ActivityPracticeTest.java
@@ -1,11 +1,61 @@
 package com.pk.dmv.activity;
 
+import com.pk.dmv.R;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ActivityPracticeTest extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		getMenuInflater().inflate(R.menu.activity_practice_test, menu);
+		
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch (item.getItemId())
+		{
+			case android.R.id.home:
+				Exit();
+				return true;
+			case R.id.action_info:
+				// TODO Open information dialog
+				Toast.makeText(this, "Information stuff goes here.", Toast.LENGTH_SHORT).show();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event)
+	{
+		if (keyCode == KeyEvent.KEYCODE_BACK)
+		{
+			Exit();
+			return true;
+		}
+		
+		return super.onKeyDown(keyCode, event);
+	}
+	
+	private void Exit()
+	{
+		finish();
+		overridePendingTransition(R.anim.fslide_left_in, R.anim.fslide_right_out);
 	}
 }
